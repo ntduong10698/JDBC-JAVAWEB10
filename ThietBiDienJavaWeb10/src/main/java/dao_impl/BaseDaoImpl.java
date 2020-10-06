f2ac0d326fdac1a045c38bb7df3c903baf84e589
ntduong10698 <44547589+ntduong10698@users.noreply.github.com>
2020-10-06 21:05:43 +0700

    B5-DaoImpl

---
+++ ThietBiDienJavaWeb10/src/main/java/dao_impl/BaseDaoImpl.java
@@ -0,0 +1,22 @@
+package dao_impl;
+
+import dao.BaseDao;
+import model.Category;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class BaseDaoImpl<T> implements BaseDao<T> {
+    public List<T> getList(ResultSet resultSet) throws SQLException {
+        List<T> list = new ArrayList<T>();
+        //cho con trỏ resulteSet chạy lần lượt qua các bản ghi bằng hàm .next() lần đầu tiên khi
+        //.next() con trỏ resultSet mới được trỏ vào bản ghi đầu tiền
+        while (resultSet.next()) {
+            T t = getObject(resultSet);
+            if(t != null) list.add(t);
+        }
+        return list;
+    }
+}
--- ThietBiDienJavaWeb10/src/main/java/model/MyConnection.java
+++ ThietBiDienJavaWeb10/src/main/java/model/MyConnection.java
@@ -13,7 +13,7 @@ public class MyConnection {
      * Các bước để kết nối đến database
      * B1: Kiểm tra dirver jdbc đã tồn tại hay chưa bằng hàm driverTest();
      * B2: thực hiện kết nối với db bằng hàm connectDB cần có các tham số đầu vào url, username, passowrd;
-     * B3: Dùng prepar và pareparUpdate để thực hiện các câu lệnh thao tác với db
+     * B3: Dùng prepare và pareparUpdate để thực hiện các câu lệnh thao tác với db
      * B4: đóng kết nối
      * */
 
